6b081a5b252db7d7651b68e6a3fb43646568765d
Barikata1984 <franche1984@gmail.com>
2021-01-25 15:49:25 -0500

    l6: show the number of max episode steps l34-35: show the scores for the random and simple policies, respectively

--- cartPoleTest.py
+++ cartPoleTest.py
@@ -3,6 +3,7 @@ import gym
 # With '-v1', the environemnt comes with 500 timesteps.
 # In contrast, the environment with '-v2' comes with 200 timesteps.
 env = gym.make('CartPole-v1')
+print(env._max_episode_steps)
 
 def select_action_random(state):
     if random() < 0.5:
@@ -19,15 +20,19 @@ def select_action_simple(state):
 def goodness_score(select_action, num_episodes = 100):
     num_steps = 500
     ts = []
-    for episode in range(num_episode):
-        for t in range(1, num_step + 1):
+    for episode in range(num_episodes):
+        state = env.reset()
+        for t in range(1, num_steps + 1):
             action = select_action(state)
             state, _, done, _ = env.step(action)
             if done:
                 break
         ts.append(t)
-    score = sum(ts) / len(ts) * num_steps)
+    score = sum(ts) / (len(ts) * num_steps)
     return score
+
+print("Score for the RANDOM policy: %f" % goodness_score(select_action_random))
+print("Score for the SIMPLE policy: %f" % goodness_score(select_action_simple))
 
 while True:
     action = select_action(state)
