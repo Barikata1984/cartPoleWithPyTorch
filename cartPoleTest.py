8892a43a92a35bc06a0515fa0a715212208a58a1
Barikata1984 <franche1984@gmail.com>
2021-01-25 15:27:09 -0500

    l6-17: add two functions to select an action give the current state l19-30: add a fuction to evaluate the action based on its reward l32-37: add a loop of a fundamental concept of reinforcement learning

--- cartPoleTest.py
+++ cartPoleTest.py
@@ -3,3 +3,36 @@ import gym
 # With '-v1', the environemnt comes with 500 timesteps.
 # In contrast, the environment with '-v2' comes with 200 timesteps.
 env = gym.make('CartPole-v1')
+
+def select_action_random(state):
+    if random() < 0.5:
+        return 0
+    else:
+        return 1
+
+def select_action_simple(state):
+    if random() < 0.5:
+        return 0
+    else:
+        return 1
+
+def goodness_score(select_action, num_episodes = 100):
+    num_steps = 500
+    ts = []
+    for episode in range(num_episode):
+        for t in range(1, num_step + 1):
+            action = select_action(state)
+            state, _, done, _ = env.step(action)
+            if done:
+                break
+        ts.append(t)
+    score = sum(ts) / len(ts) * num_steps)
+    return score
+
+while True:
+    action = select_action(state)
+    staet, _, done, _ = env.step()
+    env.render()
+    if done:
+        break
+
