2fd0297ee35dcc04ad9c34b6ec30d4154fb5e3ba
Barikata1984 <franche1984@gmail.com>
2021-01-24 23:56:49 -0500

    l3-5: add comments and creat env

--- cartPoleTest.py
+++ cartPoleTest.py
@@ -1,2 +1,5 @@
 import gym
-env = gym.make('CartPole-v1')   # With '-v1', the environemnt comes with 500 timesteps. In contrast, the environment with '-v2' comes with 200 timesteps.
+
+# With '-v1', the environemnt comes with 500 timesteps.
+# In contrast, the environment with '-v2' comes with 200 timesteps.
+env = gym.make('CartPole-v1')
