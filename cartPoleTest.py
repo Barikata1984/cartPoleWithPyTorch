e41471149357d66a661127d59a67202a78d5987d
Barikata1984 <franche1984@gmail.com>
2021-01-26 15:41:14 -0500

    l47: changed the location of "scoreHistory.append(t)"

--- cartPoleTest.py
+++ cartPoleTest.py
@@ -37,16 +37,17 @@ def select_action_good(state):
 
 def goodness_score(select_action, num_episodes = 100):
     num_steps = 500
-    ts = []
+    scoreHistory = []
     for episode in range(num_episodes):
         state = env.reset()
         for t in range(1, num_steps + 1):
             action = select_action(state)
             state, _, done, _ = env.step(action)
             if done:
+                scoreHistory.append(t)
                 break
-        ts.append(t)
-    score = sum(ts) / (len(ts) * num_steps)
+#        scoreHistory.append(t)
+    score = sum(scoreHistory) / (len(scoreHistory) * num_steps)
     return score
 
 print("Score for the RANDOM policy: %f" % goodness_score(select_action_random))
